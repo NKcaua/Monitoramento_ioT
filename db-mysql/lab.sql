47e90bdb56af6e54c9a09dc88cc76c1cb3e2d1d2
NKcaua <163947899+NKcaua@users.noreply.github.com>
2026-05-29 15:41:48 -0300

    Add files via upload

--- db-mysql/lab.sql
+++ db-mysql/lab.sql
@@ -1,5 +1,8 @@
-CREATE DATABASE iot_leitura;
-CREATE TABLE dashboard_monitoramento (
+CREATE DATABASE IF NOT EXISTS iot_leitura;
+
+USE iot_leitura;
+
+CREATE TABLE IF NOT EXISTS dashboard_monitoramento (
     id_dashboard INT AUTO_INCREMENT PRIMARY KEY,
     laboratorio VARCHAR(100),
     media_temperatura DECIMAL(5,2),
@@ -8,4 +11,45 @@ CREATE TABLE dashboard_monitoramento (
     total_leituras INT,
     data_referencia DATE,
     atualizado_em DATETIME
+);
+
+DELETE FROM dashboard_monitoramento;
+
+INSERT INTO dashboard_monitoramento
+(
+    laboratorio,
+    media_temperatura,
+    media_umidade,
+    total_presencas,
+    total_leituras,
+    data_referencia,
+    atualizado_em
+)
+VALUES
+(
+    'Lab 01',
+    27,
+    55,
+    1,
+    1,
+    CURDATE(),
+    NOW()
+),
+(
+    'Lab 02',
+    26,
+    60,
+    0,
+    1,
+    CURDATE(),
+    NOW()
+),
+(
+    'Lab 03',
+    28,
+    50,
+    1,
+    1,
+    CURDATE(),
+    NOW()
 );
